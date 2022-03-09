81e76a50073de4bce767c461b072d2b3b50c42be
Leon Li <byteflow777@gmail.com>
2022-03-09 16:17:12 +0800

    [ADD] add generic Stack

--- structures/stack/stack.go
+++ structures/stack/stack.go
@@ -1,31 +1,59 @@
 package stack
 
-type Stack struct {
-	items []interface{}
+import "fmt"
+
+type Stack[T any] struct {
+	elements     []T
+	elementCount int
 }
 
-// NewStack return a new queue
-func NewStack() *Stack {
-	return &Stack{
-		items: []interface{}{},
+// NewStack return a new Stack
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{}
+}
+
+// Push an element to the top of Stack
+func (stk *Stack[T]) Push(element T) {
+	stk.elements = append(stk.elements, element)
+	stk.elementCount++
+}
+
+// typeVal return default val of type T
+func (stk *Stack[T]) typeVal() (val T) {
+	return val
+}
+
+// Pop the top element of Stack and remove it
+// If stk.Size() == 0 mean no element in Stack, return default value of type T
+func (stk *Stack[T]) Pop() (T, bool) {
+	if stk.Size() == 0 {
+		return stk.typeVal(), false
 	}
+	stk.elementCount--
+	topElement := stk.elements[stk.elementCount]
+	stk.elements = stk.elements[:stk.elementCount]
+	return topElement, true
 }
 
-func (q *Stack) Push(item interface{}) {
-	q.items = append(q.items, item)
-}
-
-func (q *Stack) Pop() (interface{}, bool) {
-	n := q.Len()
-	if n == 0 {
-		return nil, false
+// Top return the top element of Stack, but don't remove it
+func (stk *Stack[T]) Top() (T, bool) {
+	if stk.Size() == 0 {
+		return stk.typeVal(), false
 	}
-	ans := q.items[n-1]
-	q.items = q.items[0 : n-1]
-
-	return ans, true
+	return stk.elements[stk.elementCount], true
 }
 
-func (q Stack) Len() int {
-	return len(q.items)
+// Size return the number of Stack's elements
+func (stk *Stack[T]) Size() int {
+	return stk.elementCount
+}
+
+// Empty return whether Stack is nil
+// true mean stack is nil, false mean not
+func (stk *Stack[T]) Empty() bool {
+	return stk.elementCount == 0
+}
+
+func (stk *Stack[T]) String() string {
+	return fmt.Sprintf("stack%v", stk.elements)
 }
--- structures/stack/stack_test.go
+++ structures/stack/stack_test.go
@@ -5,34 +5,37 @@ import (
 )
 
 func TestNewStack(t *testing.T) {
-	stack := NewStack()
-	if stack.items == nil {
-		t.Error("stack's inner items should be not nil")
-	}
+	stk := NewStack[int]()
+	stk.Push(1)
+	stk.Pop()
+	stk.Top()
+	stk.Size()
+	stk.Empty()
+	stk.typeVal()
 }
 
 func TestStack_Push(t *testing.T) {
-	stack := NewStack()
+	stack := NewStack[int]()
 	stack.Push(1)
 	stack.Push(2)
 	stack.Push(3)
 
 	for i := 1; i <= 3; i++ {
-		if stack.items[i-1] != i {
-			t.Errorf("stack's items: %v, excepted: [1 2 3]", stack.items)
+		if stack.elements[i-1] != i {
+			t.Errorf("stack's elements: %v, excepted: [1 2 3]", stack.elements)
 		}
 	}
 }
 
 func TestStack_Pop(t *testing.T) {
-	stack := NewStack()
+	stack := NewStack[int]()
 	stack.Push(1)
 	stack.Push(2)
 	stack.Push(3)
 
 	for i := 1; i <= 3; i++ {
-		if stack.items[i-1] != i {
-			t.Errorf("stack's items: %v, excepted: [1 2 3]", stack.items)
+		if stack.elements[i-1] != i {
+			t.Errorf("stack's elements: %v, excepted: [1 2 3]", stack.elements)
 		}
 	}
 	top, ok := stack.Pop()
@@ -46,31 +49,31 @@ func TestStack_Pop(t *testing.T) {
 		t.Errorf("got item: %v, excepted: %v", top, 3)
 	}
 
-	// pop all items
+	// pop all elements
 	for i := 0; i < 3; i++ {
 		stack.Pop()
 	}
 
 	top, ok = stack.Pop()
-	if ok || top != nil {
+	if ok || top != stack.typeVal() {
 		t.Errorf("got item: %v, excepted: %v", top, 3)
 	}
 }
 
 func TestStack_Len(t *testing.T) {
-	stack := NewStack()
+	stack := NewStack[int]()
 	stack.Push(1)
 	stack.Push(2)
 	stack.Push(3)
 
-	if n := stack.Len(); n != 3 {
+	if n := stack.Size(); n != 3 {
 		t.Errorf("got len: %v, excepted: %v", n, 3)
 	}
 
 	for i := 0; i < 3; i++ {
 		stack.Pop()
 	}
-	if n := stack.Len(); n != 0 {
+	if n := stack.Size(); n != 0 {
 		t.Errorf("got len: %v, excepted: %v", n, 0)
 	}
 }
